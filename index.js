47e7356a92b22d7497f0dc97c5e59c746d8e0b20
Ramim khan <ramimkhan>
2025-05-01 20:02:06 +0600

    wellcome javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+// if(13 > 10){
+//     console.log("My Name nafi");
+    
+// }
+var applePrice = 300;
+if(applePrice = 300){
+    console.log("i will buy");
+    
+}
+else{
+    console.log("i will not buy");
+    
+}
+
