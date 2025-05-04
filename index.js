18d8f6a584afbbcb594d02818b24ada76cce6d4b
Ramim khan <ramimkhan>
2025-05-04 20:55:56 +0600

    Welcome javascript

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// if(13 > 10){
-//     console.log("My Name nafi");
+if(13 > 10){
+    console.log("My Name nafi");
     
-// }
+}
 var applePrice = 300;
 if(applePrice = 300){
     console.log("i will buy");
@@ -11,4 +11,60 @@ else{
     console.log("i will not buy");
     
 }
+
+const applePrice = 300;
+const orangePrice = 300;
+const bananaPrice = 250;
+
+if ((applePrice <= orangePrice && bananaPrice > applePrice) || bananaPrice > orangePrice) {
+    console.log("i will buy");
+    
+}
+
+else{
+    console.log("i will go my home");
+    
+}
+
+const price = 4000;
+if(price < 5000){
+    const discount = price *10 / 100
+    const payAmount = price + discount
+    console.log(payAmount);
+    
+}
+
+const age =52;
+const biriyaniPrice = 600;
+
+if(age < 51){
+    console.log("you can eat for free");
+    
+}
+else if(age < 60){
+    const discount = biriyaniPrice *10 / 100;
+    const payAmount = biriyaniPrice - discount;
+    console.log(payAmount);
+    
+}
+
+-------------Tarnary------------------
+
+const age = 19;
+if (age >= 18) {
+    console.log("you can vote");
+    
+}
+else{
+    console.log("you can't vote");
+    
+}
+
+age >= 18 ? console.log('you can vote') : console.log("you can't vote");
+const friends = ['salam', 'nafi', 'kafi', 'ulbul']
+  
+
+console.log(friends.includes('ramim'));
+
+
 
